09ff6d9a4832e79b4ecd3ef3f9ae2b2e21235d6e
tm82reji <tm82reji@studserv.uni-leipzig.de>
2022-05-19 21:08:41 +0200

    datatypes in settings adjusted

--- classes/settings.py
+++ classes/settings.py
@@ -1,21 +1,21 @@
 class Settings:
-    def __init__(self, parser): # todo datatypes
+    def __init__(self, parser):
         self.mode = parser.mode
         if self.mode == "graph_calc":
-            self.record_count = parser.record_count
-            self.threshold = parser.threshold
-            self.removed_plain_record_frac = parser.remove_frac_plain
-            self.histo_features = parser.histo_features
-            self.analysis = parser.analysis
+            self.record_count = int(parser.record_count)
+            self.threshold = float(parser.threshold)
+            self.removed_plain_record_frac = float(parser.remove_frac_plain)
+            self.histo_features = bool(parser.histo_features)
+            self.analysis = bool(parser.analysis)
         else:
             self.pickle_file = parser.pickle_file
 
         self.results_path = parser.results_path
-        self.lsh_count = parser.lsh_count
-        self.lsh_size = parser.lsh_size
-        self.cos_sim_thold = 0.3
-        self.hyperplane_count = 1024
-        self.num_top_pairs = [10, 50, 100, 500, 1000]
-        self.vidange_sim = True
+        self.lsh_count = int(parser.lsh_count)
+        self.lsh_size = int(parser.lsh_size)
+        self.cos_sim_thold = float(0.3)
+        self.hyperplane_count = int(1024)
+        self.num_top_pairs = eval('[10, 50, 100, 500, 1000]')
+        self.vidange_sim = bool(True)
         self.graph_matching_tech = parser.graph_matching_tech
-        self.min_edges = parser.min_edges
+        self.min_edges = int(parser.min_edges)
